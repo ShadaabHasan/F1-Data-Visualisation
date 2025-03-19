f92c5e88ac4aac98d06a77326aeb31d5396ba368
ShadaabHasan <112100669+ShadaabHasan@users.noreply.github.com>
2025-03-19 05:42:59 +0530

    2021 season visualized on points

--- F1-Data-Visualisation.R
+++ F1-Data-Visualisation.R
@@ -16,14 +16,14 @@ Data_Drivers_Points_Total <- data.frame(sqldf("select a.driverRef as Driver, sum
          limit 10"))
 view(Data_Drivers_Points_Total)
 
-ggplot(Data_Drivers_Points_Total, aes(x = Driver, y = Points_Total)) +
-  geom_bar(stat = "identity", fill="blue") +
+ggplot(Data_Drivers_Points_Total, aes(x = Driver, y = Points_Total, fill=Driver)) +
+  geom_bar(stat = "identity") +
   coord_flip() +
   labs(title = "Top 10 F1 Drivers by Total Points", x = "Driver", y = "Total Points")
 
 #Reordering the graph in descending order of points
-ggplot(Data_Drivers_Points_Total, aes(x = reorder(Driver,Points_Total), y = Points_Total)) +
-  geom_bar(stat = "identity", fill = "blue") +
+ggplot(Data_Drivers_Points_Total, aes(x = reorder(Driver,Points_Total), y = Points_Total, fill=Driver)) +
+  geom_bar(stat = "identity") +
   coord_flip() +
   labs(title = "Top 10 F1 Drivers by Total Points", x = "Driver", y = "Total Points")
 
@@ -41,7 +41,7 @@ Most_wins <- data.frame(sqldf("select b.driverID as DriverId,  c.driverRef as Dr
                                         order by Total_wins DESC
                                         LIMIT 10"))
 view(Most_wins)
-ggplot(Most_wins, aes(x=reorder(Driver, Total_wins), y=Total_wins))+
+ggplot(Most_wins, aes(x=reorder(Driver, Total_wins), y=Total_wins, fill=Driver))+
   geom_histogram(stat="identity")+coord_flip()+ 
   labs(x="Driver", y="Number of Wins", title= "Top 10 drivers by most number of wins")
 
@@ -84,6 +84,7 @@ ggplot(Most_wins, aes(x=reorder(Driver, Total_wins), y=Total_wins))+
                                            from lap_times c
                                            left join drivers a on a.driverId = c.driverId
                                            left join races b on b.raceId=c.raceId
+                                           where b.circuitId IN (1,17,22,3,77,79,21,6,4,7,70,9,13,11,39,14,73,15,69,32,18,80,78,24)
                                            group by circuitId
                                            order by Fastest_LapTime Asc
                                            "))  
@@ -94,11 +95,11 @@ ggplot(Most_wins, aes(x=reorder(Driver, Total_wins), y=Total_wins))+
   view(laptime_distribution_final)  
   ggplot(laptime_distribution_final, aes(x=RaceName, y=Fastest_LapTime, label=Driver, color=Driver))+
     geom_point()+ theme(axis.text.x = element_text(angle = 55, hjust = 1))+ 
-    labs(x="Circuit", y="Fastest Lap")
+    labs(x="Circuit", y="Fastest Lap", title = "Fastest Laptimes on each circuit in F1")
   
   #Finding Drivers dominance over tracks based on the circuits in the 2025 race calendar
   circuit_wins <- data.frame(sqldf("select a.circuitId as CircuitId, b.driverID as DriverId,  c.driverRef as Driver,
-                                        d.circuitRef as Circuit_Name,
+                                        d.circuitRef as Circuit_Name, a.Name as RaceName,
                                         count(*) as Total_wins 
                                         from races a
                                         left join results b on a.raceId=b.raceId
@@ -111,15 +112,33 @@ ggplot(Most_wins, aes(x=reorder(Driver, Total_wins), y=Total_wins))+
   view(circuit_wins)  
   str(circuit_wins)
 
-  max_wins <- data.frame(sqldf("select a.CircuitId, a.Driver
-                               ,a.Circuit_Name,max(a.Total_wins) as MaxWin
+  max_wins <- data.frame(sqldf("select a.CircuitId, a.Driver,
+                               a.Circuit_Name,max(a.Total_wins) as MaxWin, a.RaceName
                                from circuit_wins a
                                where a.circuitId IN (1,17,22,3,77,79,21,6,4,7,70,9,13,11,39,14,73,15,69,32,18,80,78,24)
                                group by a.circuitId"))
   view(max_wins)
-  ggplot(max_wins, aes(x=Circuit_Name, y=MaxWin, label=Driver, fill=Driver))+
+  ggplot(max_wins, aes(x=RaceName, y=MaxWin, label=Driver, fill=Driver))+
     geom_bar(stat = "identity")+scale_fill_brewer(palette = "Set1")+
-    theme(axis.text.x = element_text(angle = 90))
+    theme(axis.text.x = element_text(angle = 90))+labs(x="Circuit", y="Number of Wins", title="Circuit dominance on 2025 circuits")
   
   
-  
+#Plotting the 2021 season of each driver and their positions in each race
+  driver_standings <- read_csv("F1 data/driver_standings.csv")
+  view(driver_standings)
+  season_2021 <- data.frame(sqldf("Select a.year, a.name as RaceName, a.raceId as RaceId,
+                                  b.position as position, b.points as Points, C.driverRef as driver, c.driverId
+                                  from races a
+                                  left join driver_standings b on b.raceId=a.raceId
+                                  left join drivers c on c.driverId=b.driverId
+                                  where year=2021
+                                  order by RaceId"))
+  view(season_2021)  
+  driver_colors <- c("#8B0000", "#000075", "#4DAF4A", "#984EA3", "#1F78B4", 
+                     "#FFFF33", "#A65628", "#F781BF", "#FF7F00", "#66C2A5",
+                     "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854", "#FFD92F", 
+                     "#E5C494", "#B3B3B3", "#999999", "#33A02C", "#FB9A99","#E41A1C")
+  ggplot(season_2021, aes(x=reorder(RaceName, Points), y=Points,group=driver, colour = driver))+
+    geom_line()+geom_point()+labs(x="Race",y="Points", title="2021 F1 Season")+
+    scale_color_manual(values=driver_colors)+
+    theme(axis.text.x = element_text(angle = 55, hjust=1))
